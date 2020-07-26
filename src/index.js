0c2692e76d2e40fd6251f075dbf85593fe78c6ba
Oleksandr Bevzytskyi <bealkl@gmail.com>
2020-07-26 18:26:56 +0300

    make build

---
+++ src/index.js
@@ -0,0 +1,38 @@
+'use strict';
+
+// import '@pnotify/core/dist/BrightTheme.css';
+import _ from 'lodash';
+import PNotify from '@pnotify/core/dist/PNotify.js';
+
+const refs = {
+  inputCountry: document.querySelector('#inputCountry'),
+  listContry: document.querySelector('#listCountry'),
+};
+refs.inputCountry.addEventListener('input', _.debounce(fetchCountries, 500));
+
+function fetchCountries() {
+  if (refs.inputCountry.value == '') return;
+  fetch(`https://restcountries.eu/rest/v2/name/${refs.inputCountry.value}`)
+    .then(result => result.json())
+    .then(data => showCountries(data));
+}
+
+function showCountries(items) {
+  refs.listContry.innerHTML = '';
+  if (items.length > 10) {
+    PNotify.alert({
+      title: 'Запрос не найден!',
+      text: 'Пожалуйста, введите более точный запрос.',
+    });
+    return;
+  }
+  if (items.length === 1) {
+    const langList = items[0].languages.map(item => `<li>${item.name}</li>`).join('');
+    refs.listContry.insertAdjacentHTML(
+      'beforeend',
+      `<h1>${items[0].name}</h1><div id="countryBlock"><div id="infoCountry"><p><b>Capital: </b>${items[0].capital}</p><p><b>Population: </b>${items[0].population}</p><ul><b>Languages: </b>${langList}</ul></div><img src="${items[0].flag}" alt="${items[0].capital}_flag"></div>`,
+    );
+  } else {
+    refs.listContry.insertAdjacentHTML('beforeend', items.map(item => `<li>${item.name}</li>`).join(''));
+  }
+}
